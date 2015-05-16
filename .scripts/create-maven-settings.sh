ce578e792f4f27ae9377f2d760326e6b89e6f7b6
Bernardo Martínez Garrido <programming@wandrell.com>
2015-05-16 12:02:50 +0200

    Corrected Maven settings script.

--- .scripts/create-maven-settings.sh
+++ .scripts/create-maven-settings.sh
@@ -8,25 +8,25 @@
    echo "<servers>";
    # Releases FTP
    echo "<server>";
-      echo "<id>\${env.REPO_RELEASES_ID}</id>";
+      echo "<id>releases</id>";
       echo "<username>\${env.REPO_RELEASES_USER}</username>";
       echo "<password>\${env.REPO_RELEASES_PASSWORD}</password>";
    echo "</server>";
    # DEVELOP FTP
    echo "<server>";
-      echo "<id>\${env.REPO_DEVELOP_ID}</id>";
+      echo "<id>snapshots</id>";
       echo "<username>\${env.REPO_DEVELOP_USER}</username>";
       echo "<password>\${env.REPO_DEVELOP_PASSWORD}</password>";
    echo "</server>";
    # Site FTP
    echo "<server>";
-      echo "<id>\${env.REPO_SITE_ID}</id>";
+      echo "<id>site</id>";
       echo "<username>\${env.REPO_SITE_USER}</username>";
       echo "<password>\${env.REPO_SITE_PASSWORD}</password>";
    echo "</server>";
    # Development site FTP
    echo "<server>";
-      echo "<id>\${env.REPO_SITE_DEVELOP_ID}</id>";
+      echo "<id>site-development</id>";
       echo "<username>\${env.REPO_SITE_DEVELOP_USER}</username>";
       echo "<password>\${env.REPO_SITE_DEVELOP_PASSWORD}</password>";
    echo "</server>";
