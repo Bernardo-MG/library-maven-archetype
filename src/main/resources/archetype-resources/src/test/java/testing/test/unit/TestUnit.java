740a00667145ffc25b6323624fd12794e4558dba
Bernardo Martínez Garrido <programming@wandrell.com>
2015-06-24 23:26:05 +0200

    Added javadocs to the placeholder classes.

--- src/main/resources/archetype-resources/src/test/java/testing/test/unit/TestUnit.java
+++ src/main/resources/archetype-resources/src/test/java/testing/test/unit/TestUnit.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2014 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package ${package}.testing.test.unit;
 
 import org.testng.Assert;
@@ -5,12 +28,31 @@ import org.testng.annotations.Test;
 
 import ${package}.Greeter;
 
+/**
+ * Unit tests for {@link Greeter}.
+ * <p>
+ * Checks the following cases:
+ * <ol>
+ * <li>The {@code Greeter} returns a greeting.</li>
+ * </ol>
+ * <p>
+ * This is just a placeholder test.
+ * 
+ * @author ${developerName}
+ *
+ */
 public final class TestUnit {
 
+    /**
+     * Constructs the tests.
+     */
     public TestUnit() {
         super();
     }
 
+    /**
+     * Tests that the {@code Greeter} returns a greeting.
+     */
     @Test
     public final void sayHello() {
         Assert.assertEquals("Hello World!", new Greeter().sayHello());
